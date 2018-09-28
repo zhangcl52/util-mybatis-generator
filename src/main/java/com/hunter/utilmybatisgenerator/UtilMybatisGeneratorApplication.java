508e26b98ea30e67af8f99aff47a7d476713455e
zhangcl <zhangchlo@sina.cn>
2018-09-28 20:27:53 +0800

    first commit

---
+++ src/main/java/com/hunter/utilmybatisgenerator/UtilMybatisGeneratorApplication.java
@@ -0,0 +1,13 @@
+package com.hunter.utilmybatisgenerator;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class UtilMybatisGeneratorApplication {
+
+    public static void main(String[] args) {
+
+        SpringApplication.run(UtilMybatisGeneratorApplication.class, args);
+    }
+}
---
+++ src/test/java/com/atour/utilmybatisgenerator/UtilMybatisGeneratorApplicationTests.java
@@ -0,0 +1,16 @@
+package com.atour.utilmybatisgenerator;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class UtilMybatisGeneratorApplicationTests {
+
+    @Test
+    public void contextLoads() {
+    }
+
+}
